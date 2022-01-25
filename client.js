614b38c590257758905bff0fcbe1f262829e928b
เอกรัฐ สุขเขียว <ln_waut@hotmail.com>
2022-01-25 14:45:32 +0700

    last  commit

--- client.js
+++ client.js
@@ -1,15 +1,38 @@
-var net = require('net');
-var HOST = '127.0.0.1';
-var PORT = 6969;
-var client = new net.Socket();
+const { connect } = require('http2')
+var net = require('net')
+var readline = require('readline-sync')
+
+var HOST = '127.0.0.1'
+var PORT = 6969
+
+var client = new net.Socket()
 client.connect(PORT, HOST, function() {
-    console.log('CONNECTED TO: ' + HOST + ':' + PORT);
-    client.write('I am Chuck Norris!');
+    console.log('CONNECTED TO: ' + HOST + ':' + PORT)
+    //client.write('sword')
 });
+//var keydata = window.prompt(" Enter Item : ");
+//let data = prompt("Enter Item : ");
 client.on('data', function(data) {
-    console.log('DATA: ' + data);
-    client.destroy();
+    //const readline = require('readline');
+    //const data = readline.createInterface({
+    //   input : process.stdin,
+    //    output : process.stdout
+    //});
+
+    console.log('ServerGameData : ' + data)
+    sendline()
+    client.destroy()
 });
-client.on('close', function()
- {console.log('Connection closed');
-});
+
+client.on('close', function() {
+    console.log('GAMEOVER!')
+})
+
+function sendline() {
+    var line = readline.question('DataSent: ')
+    if(line == 'GAMEOVER!') {
+        client.end()
+    }else {
+        client.write(line)
+    }
+}
--- server.js
+++ server.js
@@ -1,15 +1,60 @@
 var net = require('net');
 var HOST = '127.0.0.1';
 var PORT = 6969;
-var server = net.createServer();
-server.listen(PORT, HOST);
-server.on('connection', function(sock) {
-    console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);
-    sock.on('data', function(data) {
-        console.log('DATA ' + sock.remoteAddress + ': ' + data);
-        sock.write('You said "' + data + '"');
-    });
-    sock.on('close', function(data) {
-        console.log('CLOSED: ' + sock.remoteAddress +' '+ sock.remotePort);
-    });
-});
+
+var db = ["sword", "spear", "staff", "shield", "bow", "crossbow", "potion", "poison", "glove", "hat", "shoe", "apple"]
+
+net.createServer(function (sock) {
+    var state = 0
+    var current_key = null
+    var i = 0
+    sock.on('data', function (data) {
+        switch(state){
+            case 0:
+                if(data == 'STARTGAME'){
+                    sock.write('GAMESTART')
+                    state = 1 //wait for key
+                }
+                break
+            case 1:
+                current_key = data
+                //sock.write("" + (db[current_key] || 0))
+                for(i = 0 ; i<=db.length; i++) {
+                    if(db[i] == current_key) {
+                        if(i == 0){
+                            sock.write('' + (i+1))
+                        }else {
+                            sock.write('' + (i))
+                        }
+                    }
+                }
+                state = 2 //wait for number
+                break
+            case 2:
+                if(data == 'GAMEOVER!!'){
+                    sock.write('GG')
+                    sock.close()
+                    state = 3 //end
+                }else{
+                try{
+                    let v = data.toUppercase()
+                    if(!db[current_key]){
+                        db[current_key] = 0
+                        sock.write("" + (db[current_key]))
+                    //db[current_key] += v
+                    }
+
+                    for(i = 0; i < db.length; i++) {
+                        if(db[i] == v) {
+                            sock.write("" + (db[i] + 1))
+                        }
+                    }
+                }catch(e){
+                    sock.write('INVALID')
+                }
+            }
+            break
+        }
+    })
+}).listen(PORT, HOST);
+console.log('Server listening on ' + HOST + ':' + PORT);
